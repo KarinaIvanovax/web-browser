ac908cac540c57c73725e758101591593ae6e14a
Пользователь <@LAPTOP-JFIRCV9H>
2023-04-13 15:38:32 +0300

    Добавьте файлы проекта.

---
+++ web-browser/Web.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace web_browser
+{
+    partial class Web
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Web));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bookmark = new System.Windows.Forms.PictureBox();
+            this.refresh = new System.Windows.Forms.PictureBox();
+            this.next = new System.Windows.Forms.Button();
+            this.previous = new System.Windows.Forms.Button();
+            this.searcher = new System.Windows.Forms.PictureBox();
+            this.search = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.историяToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.закладкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookmark)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.refresh)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.searcher)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(229)))), ((int)(((byte)(208)))));
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.bookmark);
+            this.panel1.Controls.Add(this.refresh);
+            this.panel1.Controls.Add(this.next);
+            this.panel1.Controls.Add(this.previous);
+            this.panel1.Controls.Add(this.searcher);
+            this.panel1.Controls.Add(this.search);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(992, 442);
+            this.panel1.TabIndex = 1;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(229)))), ((int)(((byte)(208)))));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 429);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(992, 13);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // bookmark
+            // 
+            this.bookmark.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bookmark.Image = ((System.Drawing.Image)(resources.GetObject("bookmark.Image")));
+            this.bookmark.Location = new System.Drawing.Point(811, 240);
+            this.bookmark.Name = "bookmark";
+            this.bookmark.Size = new System.Drawing.Size(47, 46);
+            this.bookmark.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bookmark.TabIndex = 5;
+            this.bookmark.TabStop = false;
+            this.toolTip1.SetToolTip(this.bookmark, "добавить в закладки");
+            // 
+            // refresh
+            // 
+            this.refresh.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.refresh.Image = ((System.Drawing.Image)(resources.GetObject("refresh.Image")));
+            this.refresh.Location = new System.Drawing.Point(182, 237);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(47, 49);
+            this.refresh.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.refresh.TabIndex = 1;
+            this.refresh.TabStop = false;
+            this.toolTip1.SetToolTip(this.refresh, "обновить страницу");
+            // 
+            // next
+            // 
+            this.next.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.next.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.next.Location = new System.Drawing.Point(111, 240);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(65, 46);
+            this.next.TabIndex = 4;
+            this.next.Text = ">>";
+            this.toolTip1.SetToolTip(this.next, "следующая страница");
+            this.next.UseVisualStyleBackColor = true;
+            // 
+            // previous
+            // 
+            this.previous.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.previous.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.previous.Location = new System.Drawing.Point(40, 240);
+            this.previous.Name = "previous";
+            this.previous.Size = new System.Drawing.Size(65, 46);
+            this.previous.TabIndex = 3;
+            this.previous.Text = "<<";
+            this.toolTip1.SetToolTip(this.previous, "предыдущая страница");
+            this.previous.UseVisualStyleBackColor = true;
+            this.previous.Click += new System.EventHandler(this.previous_Click);
+            // 
+            // searcher
+            // 
+            this.searcher.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.searcher.Image = ((System.Drawing.Image)(resources.GetObject("searcher.Image")));
+            this.searcher.Location = new System.Drawing.Point(756, 240);
+            this.searcher.Name = "searcher";
+            this.searcher.Size = new System.Drawing.Size(49, 46);
+            this.searcher.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.searcher.TabIndex = 2;
+            this.searcher.TabStop = false;
+            this.toolTip1.SetToolTip(this.searcher, "поиск");
+            this.searcher.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // search
+            // 
+            this.search.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.search.Location = new System.Drawing.Point(235, 237);
+            this.search.Multiline = true;
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(515, 49);
+            this.search.TabIndex = 1;
+            this.search.Enter += new System.EventHandler(this.search_Enter);
+            this.search.Leave += new System.EventHandler(this.search_Leave);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(188)))), ((int)(((byte)(194)))));
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.menuStrip1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(992, 112);
+            this.panel2.TabIndex = 0;
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseDown);
+            this.panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseMove);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(251)))), ((int)(((byte)(231)))));
+            this.label2.Location = new System.Drawing.Point(953, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 41);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "X";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(251)))), ((int)(((byte)(231)))));
+            this.label1.Location = new System.Drawing.Point(393, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 68);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Поиск...";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(992, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.Visible = false;
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.историяToolStripMenuItem,
+            this.закладкиToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(32, 24);
+            this.toolStripMenuItem1.Text = "...";
+            // 
+            // историяToolStripMenuItem
+            // 
+            this.историяToolStripMenuItem.Name = "историяToolStripMenuItem";
+            this.историяToolStripMenuItem.Size = new System.Drawing.Size(155, 26);
+            this.историяToolStripMenuItem.Text = "История";
+            // 
+            // закладкиToolStripMenuItem
+            // 
+            this.закладкиToolStripMenuItem.Name = "закладкиToolStripMenuItem";
+            this.закладкиToolStripMenuItem.Size = new System.Drawing.Size(155, 26);
+            this.закладкиToolStripMenuItem.Text = "Закладки";
+            // 
+            // Web
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(992, 442);
+            this.Controls.Add(this.panel1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Web";
+            this.Text = "Web";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookmark)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.refresh)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.searcher)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private PictureBox bookmark;
+        private PictureBox refresh;
+        private Button next;
+        private Button previous;
+        private PictureBox searcher;
+        private TextBox search;
+        private Panel panel2;
+        private Label label1;
+        private ToolTip toolTip1;
+        private Label label2;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ToolStripMenuItem историяToolStripMenuItem;
+        private ToolStripMenuItem закладкиToolStripMenuItem;
+        private DataGridView dataGridView1;
+        private BindingSource bindingSource1;
+    }
+}
---
+++ web-browser/Web.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace web_browser
+{
+    public partial class Web : Form
+    {
+
+        public Web()
+        {
+            InitializeComponent();
+        }
+
+        private void search_Enter(object sender, EventArgs e)
+        {
+            if (search.Text == "Введите URL адрес...")
+            {
+                search.Text = "";
+                search.ForeColor = Color.Black;
+            }
+        }
+
+        private void search_Leave(object sender, EventArgs e)
+        {
+            if (search.Text == "")
+            {
+                search.Text = "Введите URL адрес...";
+                search.ForeColor = Color.Gray;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(search.Text) || search.Text.Length > search.MaxLength)
+            {
+                MessageBox.Show("Не удалось отправить запрос!");
+            }
+            else
+            {
+                
+            }
+        }
+
+       
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        Point lastPoint;
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void panel2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void panel2_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void previous_Click(object sender, EventArgs e)
+        {
+           // Web.GoBack();
+        }
+    }
+}
